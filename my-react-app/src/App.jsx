c3d3bcd8a409dbf2171f0745fc74b390f1128770
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-09 02:10:12 +0600

    2/9/25

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,14 @@
 import Header from './Header.jsx'
+import Footer  from './Footer.jsx'
 
 function App() {
   return (
-    <Header/>
+    <>
+
+      <Header/>
+      <Footer/>
+      
+    </>
   );
 }
 
