bc38f19fa3bb0061f300708ab461a04bded72f45
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-10 01:31:23 +0600

    2/10/25

---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,15 @@
+function Food(){
+    const f1="Pizza";
+    const f2="Burger";
+    const f3="Coke";
+
+    return(
+        <ul>
+            <li>Apple</li>
+            <li>{f1}</li>
+            <li>{f2.toUpperCase()}</li>
+        </ul>
+    );
+}
+
+export default Food;
