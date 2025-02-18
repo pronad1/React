bbd56595739f0f259de36a3b5d2ecaafe56cd681
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-19 02:09:15 +0600

    2/19/25

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,9 @@
+function Card() {
+  return (
+    <div className="card">
+      <img src="https://via.placeholder.com/150" alt="placeholder" />
+      <h2>Card Title</h2>
+      <p>Card Description</p>
+    </div>
+  );
+} 
